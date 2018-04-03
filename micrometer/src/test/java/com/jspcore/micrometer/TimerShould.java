241a10d91f817591e8f72eeee4674a7c28e04944
MuSTa1nE <mustainer@gmail.com>
2018-04-03 23:02:08 +0100

    Add learning tests for micrometer.

---
+++ micrometer/src/test/java/com/jspcore/micrometer/TimerShould.java
@@ -0,0 +1,49 @@
+package com.jspcore.micrometer;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+
+public class TimerShould {
+
+  private static final String MY_TIMER = "my.timer";
+  private final MeterRegistry registry = new SimpleMeterRegistry();
+
+  @Test
+  public void record_time_elapsed() {
+    Timer timer = Timer.builder(MY_TIMER)
+      .register(registry);
+
+    timer.record(() -> waitFor(TimeUnit.MILLISECONDS, 50));
+
+    assertThat(registry.timer(MY_TIMER).totalTime(MILLISECONDS)).isGreaterThan(50);
+  }
+
+  @Test
+  public void record_time_elapsed_when_exception() {
+    Timer timer = Timer.builder(MY_TIMER)
+      .register(registry);
+
+    catchThrowable(() -> timer.record(() -> {
+      waitFor(TimeUnit.MILLISECONDS, 50);
+      throw new RuntimeException();
+    }));
+
+    assertThat(registry.timer(MY_TIMER).totalTime(MILLISECONDS)).isGreaterThan(50);
+  }
+
+  private void waitFor(TimeUnit timeUnit, int unit) {
+    try {
+      timeUnit.sleep(unit);
+    } catch (InterruptedException e) {
+      throw new RuntimeException();
+    }
+  }
+}
